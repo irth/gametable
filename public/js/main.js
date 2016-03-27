e56e73de5f4aeb860de642674616b546fbdac996
Wojciech Kwolek <me@irth.pl>
2016-03-27 20:31:09 +0200

    Added background selector.

--- public/js/main.js
+++ public/js/main.js
@@ -44,10 +44,35 @@ xmlHttp.onreadystatechange = function() {
 		gamesViewModel.games.push.apply(gamesViewModel.games, JSON.parse(xmlHttp.responseText).map(function(game) {
 			return new Game(game.name, game.appid)
 		}));
-		
+
 	}
 }
 xmlHttp.open("GET", "/games", true); // true for asynchronous 
 xmlHttp.send(null);
 
 ko.applyBindings(gamesViewModel, document.getElementById("game-select"));
+
+function Background(miniature, full) {
+	this.miniature = miniature;
+	this.full = miniature;
+}
+
+backgroundsViewModel = {
+	backgrounds: ko.observableArray(),
+	chosenBackground: ko.observable()
+}
+
+gamesViewModel.chosenGame.subscribe(function(game) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() { 
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			backgroundsViewModel.backgrounds(JSON.parse(xmlHttp.responseText).map(function(background) {
+				return new Background(background[0], background[1])
+			}));
+		}
+	}
+	xmlHttp.open("GET", "/screenshots/" + gamesViewModel.chosenGame().appid, true); // true for asynchronous 
+	xmlHttp.send(null);
+});
+
+ko.applyBindings(backgroundsViewModel, document.getElementById("background-select"));
