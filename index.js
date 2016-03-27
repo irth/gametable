111144171e0be699db87bb89671d2fb7c7d01816
Wojciech Kwolek <me@irth.pl>
2016-03-27 12:09:53 +0200

    Added game selector.

--- index.js
+++ index.js
@@ -2,20 +2,40 @@ var express = require('express');
 var app = express();
 
 var jsdom = require("jsdom");
+var http = require("http");
+var apicache = require('apicache').middleware;
 
 app.use(express.static('public'));
 
-app.get(/\/screenshots\/\d+/, function (req, res) {
+app.get(/\/screenshots\/\d+/, apicache('1 hour'), function (req, res) {
 	id = req.url.replace('/screenshots/', '')
-	jsdom.env("http://store.steampowered.com/app/" + id,	[],	function(err, window) {
-		nodelist = window.document.querySelectorAll('.highlight_strip_screenshot > img[src]:nth-child(1)');
-		res.send(JSON.stringify(Array.prototype.map.call(nodelist, function(img) {
-			return [img.src, img.src.replace('.116x65.jpg', '.jpg')];
-		})));
+		jsdom.env("http://store.steampowered.com/app/" + id,	[],	function(err, window) {
+			nodelist = window.document.querySelectorAll('.highlight_strip_screenshot > img[src]:nth-child(1)');
+			res.send(JSON.stringify(Array.prototype.map.call(nodelist, function(img) {
+				return [img.src, img.src.replace('.116x65.jpg', '.jpg')];
+			})));
+		});
+});
+
+app.get('/games', apicache('1 hour'), function(req, res) {
+	http.get("http://api.steampowered.com/ISteamApps/GetAppList/v0001/", function(steam_res) {
+		var body = '';
+
+		steam_res.on('data', function(chunk) {
+			body += chunk;
+			console.log("got data" + new Date)
+		});
+
+		steam_res.on('end', function() {
+			var gameList = JSON.parse(body);
+			res.send(JSON.stringify(gameList["applist"]["apps"]["app"]));
+		});
+	}).on('error', function(e) {
+		console.log("Got an error: ", e);
 	});
 });
 
 app.listen(3000, function () {
-	  console.log('Listening at 3000!');
+	console.log('Listening at 3000!');
 });
 
--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,7 @@ function Weekday(name) {
 	this.name = name;
 	this.classes = ko.observableArray([]);
 	this.newclass = ko.observable("");
+	this.newclass.extend({ notify: 'always' });
 	this.addclass = function() {
 		this.classes.push(new Class(this.newclass(), this));
 		this.newclass("");
@@ -17,12 +18,36 @@ function Weekday(name) {
 
 timetableViewModel = {
 	weekdays: ko.observableArray([
-		new Weekday("Monday"),
-		new Weekday("Tuesday"),
-		new Weekday("Wednesday"),
-		new Weekday("Thursday"),
-		new Weekday("Friday"),
+								new Weekday("Monday"),
+								new Weekday("Tuesday"),
+								new Weekday("Wednesday"),
+								new Weekday("Thursday"),
+								new Weekday("Friday"),
 	])
 }
 
-ko.applyBindings(timetableViewModel, document.getElementById("timetable-editor");
+ko.applyBindings(timetableViewModel, document.getElementById("timetable-editor"));
+
+function Game(name, appid) {
+	this.name = name;
+	this.appid = appid;
+}
+
+gamesViewModel = {
+	games: ko.observableArray([]),
+	chosenGame: ko.observable()
+}
+
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function() { 
+	if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+		gamesViewModel.games.push.apply(gamesViewModel.games, JSON.parse(xmlHttp.responseText).map(function(game) {
+			return new Game(game.name, game.appid)
+		}));
+		
+	}
+}
+xmlHttp.open("GET", "/games", true); // true for asynchronous 
+xmlHttp.send(null);
+
+ko.applyBindings(gamesViewModel, document.getElementById("game-select"));
